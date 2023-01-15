edbbd25e9774b84b6deda0059a8eb175b34fb1f4
Haidar Yahfoufi <haidar.yahfoufi@gmail.com>
2023-01-14 22:06:51 -0500

    first minting implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { db } from "../src/firebase";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const form = new FormData();
+  const dataObj = [
+    { trait_type: "size", value: `${db.size}` },
+    { trait_type: "color", value: `${db.color}` },
+    { trait_type: "gender", value: `${db.gender}` },
+    { trait_type: "price", value: `${db.price}` },
+    { trait_type: "type", value: `${db.type}` },
+    { trait_type: "closure type", value: `${db.closure_type}` },
+    { trait_type: "material", value: `${db.material}` },
+  ];
+  form.append("chain", "goerli");
+  form.append("name", "preferenceNFT");
+  form.append("imageUrl", "https://ibb.co/VJTsvHm");
+  form.append("description", "This is what i look for when buying shoes");
+  form.append("recipientAddress", "0xc9D7452135E6209ce11F79B4603aFc3A90530675");
+  form.append("data", JSON.stringify(dataObj));
+
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "X-API-Key": "sk_live_183223e7-e1ba-4815-b8d0-432396faa7a9",
+    },
+  };
+
+  options.body = form;
+
+  fetch("https://api.verbwire.com/v1/nft/mint/quickMintFromMetadata", options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
+  return <>Testing</>;
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+export const db = 
+  {
+    size: 9,
+    color: "red",
+    gender: "male",
+    price: 80,
+    closure_type: "laces",
+    type: "boots",
+    material: "leather",
+  }
+;
+
